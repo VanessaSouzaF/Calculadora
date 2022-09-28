b7699bc38879f851d216e259109002402990a5f5
VanessaSouzaf <vanessacat.souza@gmail.com>
2022-09-28 10:52:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ExemplosFundamentos.Models;
+
+Calculadora calc = new Calculadora();
+
+/* calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15 , 45);
+calc.Dividir(2, 2);
+calc.Potencia(3,3); */
+/* calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30); */
+calc.RaizQuadrada(9);
+
+//int num = 10;
+/* Console.WriteLine("Incrementando o 10");
+//num = num +1;
+num++; */
+
+/* Console.WriteLine("Decrementando o 10");
+//num = num -1;
+num--;
+
+Console.WriteLine(num); */
